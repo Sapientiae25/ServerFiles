385144970005cec5b8e2beed44f7bdd39637a0a3
Vernell <vnell2005@gmail.com>
2022-03-12 10:58:20 +0000

    dsd

--- view_style.php
+++ view_style.php
@@ -4,11 +4,28 @@ if (isset($_POST['user_fk']) && isset($_POST['style_fk'])){
 
     $user_fk = $_POST['user_fk'];
     $style_fk = $_POST['style_fk'];
-    
-    $sql = "INSERT INTO viewed (user_fk,style_fk) VALUES (?,?)";
+
+    $sql = "SELECT * FROM viewed WHERE user_fk = ? AND style_fk = ?";
 
     $stmt= $conn->prepare($sql);
     $stmt->bind_param("ii", $user_fk,$style_fk);
-    $stmt->execute();}
+    $stmt->execute();
+    $stmt->store_result();
+    $num = $stmt->num_rows(); 
+
+    if ($num == 0){
+        $sql = "INSERT INTO viewed (user_fk,style_fk,view_date) VALUES (?,?,NOW())";
+        $stmt= $conn->prepare($sql);
+        $stmt->bind_param("ii", $user_fk,$style_fk);
+        $stmt->execute();
+    }
+    else{
+        $sql = "UPDATE viewed SET view_date = NOW() WHERE user_fk = ? AND style_fk = ?";
+        $stmt= $conn->prepare($sql);
+        $stmt->bind_param("ii", $user_fk,$style_fk);
+        $stmt->execute();
+    }
+
+    }
     else{echo "failed";}
 ?>
